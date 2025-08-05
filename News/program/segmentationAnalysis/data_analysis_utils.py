9323da7e10097c9a27f7df9eece0b573a2ae484f
Lince <licesgg@gmail.com>
2025-08-05 00:36:17 -0300

    [feat] `Out Of Bootstrap .362+` Enhancement

--- News/program/segmentationAnalysis/data_analysis_utils.py
+++ News/program/segmentationAnalysis/data_analysis_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+from os import path
+import typing as t
 from old.resultsAnalysis.numberWithUncertainty import numberWithUncertainty
 
 def reduce_df_dim(df:pd.DataFrame, column:str):
@@ -27,3 +28,32 @@ def quantify_condo_ds_mean_quality_prop(ds_sample, quality_prop="accuracy"):
     classification_metrics_df = reduce_df_dim(pd.DataFrame(ds_sample), "classification_evaluation_metrics")
     return classification_metrics_df[quality_prop].mean()
     # return mean_w_precision(classification_metrics_df[quality_prop])
+    
+    
+    
+def cache_result(cache_path: str, eval_cache: t.Callable[[str], any]=None):
+    def cache_return_wrapper(f: t.Callable):
+        def f_w_cache(*args, **kargs):
+            if path.exists(cache_path):
+                with open(cache_path, "r") as file:
+                    file_content = file.read()
+                    
+                    if eval_cache != None:
+                        ret = eval_cache(file_content)
+                    else:
+                        ret = eval(file_content)
+                        
+                    return ret
+
+            else: 
+                ret = f(*args, **kargs)
+                
+                with open(cache_path, "w") as file:
+                    file.write(repr(ret))
+                    
+                return ret
+            
+        return f_w_cache
+            
+    return cache_return_wrapper
+        
--- News/program/segmentationAnalysis/oob_results_sampling.py
+++ News/program/segmentationAnalysis/oob_results_sampling.py
@@ -2,46 +2,45 @@ from typing import Callable, Dict
 import numpy as np
 import pandas as pd
 import types as t
-from bootstrap_sampling import sample_ds
-from data_analysis_utils import reduce_df_dim
-from data_analysis_utils import mean_w_precision
+from bootstrap_sampling import generate_bootstrap_samples_idxs, sample_ds
+from data_analysis_utils import cache_result, reduce_df_dim, mean_w_precision
 from analysisTypes import *
-
-import line_profiler
-
-def no_info_err_rate_4_condo_ds(condo_ds):
-    classification_metrics = reduce_df_dim(pd.DataFrame(condo_ds), "classification_evaluation_metrics")
-    classification_distribution = reduce_df_dim(classification_metrics, "typesDistribution")
-    resubstitution_confusion_matrix = classification_distribution.apply(lambda x: x.sum())
-    
-    TP, TN, FP, FN = resubstitution_confusion_matrix["TP"], resubstitution_confusion_matrix["TN"], resubstitution_confusion_matrix["FP"], resubstitution_confusion_matrix["FN"]
-    actual_classes = {"P": TP + FN,  "N": FP + TN}
-    predicted_classes = {"P": TP + FP,  "N": TN + FN}   
-
-    no_info_err_rate = 0
-    for crr_class in actual_classes.keys():
-        class_proportion_in_ds = actual_classes[crr_class] / resubstitution_confusion_matrix["TOTAL_CLASSIFICATIONS"]
-        class_prediction_proportion_in_ds = predicted_classes[crr_class] / resubstitution_confusion_matrix["TOTAL_CLASSIFICATIONS"]
-        
-        no_info_err_rate += class_proportion_in_ds * (1 - class_prediction_proportion_in_ds)
-        
-    return no_info_err_rate
-
 
 def oob_362_correction(oob_err, resubstitution_err):
     return (0.368 * resubstitution_err) + (0.632 * oob_err)
 
+def no_info_err_rate_4_condo_ds(condo_ds:condo_ds_t):
+    # Get Confusion Matrix Values From condo_ds
+    classification_metrics = reduce_df_dim(pd.DataFrame(condo_ds), "classification_evaluation_metrics")
+    classification_distribution = reduce_df_dim(classification_metrics, "typesDistribution")
+    resubstitution_confusion_matrix = classification_distribution.apply(lambda col: col.sum())
+    
+    TP, TN = resubstitution_confusion_matrix["TP"], resubstitution_confusion_matrix["TN"]
+    FP, FN = resubstitution_confusion_matrix["FP"], resubstitution_confusion_matrix["FN"]
+    TOTAL_CLASSIFICATIONS = resubstitution_confusion_matrix["TOTAL_CLASSIFICATIONS"]
+    
+    actual_classes = {"P": TP + FN,  "N": FP + TN}
+    predicted_classes = {"P": TP + FP,  "N": TN + FN}   
+
+    # Compute no_info_err_rate
+    no_info_err_rate = 0
+    for crr_class in actual_classes.keys():
+        class_proportion = actual_classes[crr_class] / TOTAL_CLASSIFICATIONS
+        class_prediction_proportion_to_truth = predicted_classes[crr_class] / TOTAL_CLASSIFICATIONS
+        
+        no_info_err_rate += class_proportion * (1 - class_prediction_proportion_to_truth)
+        
+    return no_info_err_rate
+
 
 def oob_362_plus_correction(oob_err, resubstitution_err, no_info_err_rate):
-    
     relative_overfitting_rate = (oob_err - resubstitution_err) / (no_info_err_rate - resubstitution_err)
     coeff = 0.632 / (1 - 0.368 * relative_overfitting_rate)
     
     return (1 - coeff) * resubstitution_err + coeff * oob_err
 
 
-@line_profiler.profile
-def oob_362_quality_prop_measurement(dataset: t.List[any], bootstrap_samples_idxs:t.List[t.Tuple[t.List[int], t.List[int]]], props_extraction_method:Dict[str, Callable[[pd.DataFrame], pd.DataFrame]]={}):
+def oob_362_quality_prop_measurement(dataset: t.List[any], bootstrap_samples_idxs:t.List[t.Tuple[t.List[int], t.List[int]]], props_extraction_method:Dict[str, Callable[[pd.DataFrame, t.Optional[Dict]], pd.DataFrame]]={}):
         
     # Compute OOB 362 Corrected Value 
     # (Each Metric from props_extraction_method; 
@@ -55,12 +54,13 @@ def oob_362_quality_prop_measurement(dataset: t.List[any], bootstrap_samples_idx
     
     resubstitution_no_info_err_rate = no_info_err_rate_4_condo_ds(dataset)
     
+    cache = {}
     for crr_prop, get_crr_prop_from_df in props_extraction_method.items():
-        props_resubstitution[crr_prop] = get_crr_prop_from_df(dataset).mean()
+        props_resubstitution[crr_prop] = get_crr_prop_from_df(dataset, cache).mean()
         
         props_values["train"][crr_prop] = []
         props_values["test"][crr_prop] = []
-        
+    del cache
     
     for train_samples_idxs, test_samples_idxs in bootstrap_samples_idxs:
         samples = {
@@ -69,12 +69,13 @@ def oob_362_quality_prop_measurement(dataset: t.List[any], bootstrap_samples_idx
         }
         
         for type_of_sample in ["train", "test"]:
+            cache = {}
             for crr_prop, get_crr_prop_from_df in props_extraction_method.items():
-                prop_sample_mean = get_crr_prop_from_df(samples[type_of_sample]).mean()
+                prop_sample_mean = get_crr_prop_from_df(samples[type_of_sample], cache).mean()
                     
                 prop_corrected = oob_362_plus_correction(prop_sample_mean, props_resubstitution[crr_prop], resubstitution_no_info_err_rate)
                 props_values[type_of_sample][crr_prop].append(prop_corrected)
-                
+        del cache
        
     # Compute Results: OOB 362 Corrected Value Mean
     # (Each Metric from props_extraction_method; 
@@ -95,4 +96,26 @@ def oob_362_quality_prop_measurement(dataset: t.List[any], bootstrap_samples_idx
         
     return results
 
-
+def compute_oob_362_results_over_methods(color_space_oriented_df, f_to_get_metric_from_condo_ds, sample_amount=300):
+    oob_362_plus_results_over_methods = {}
+    
+    # Use Sample Indexes to Make Samples Lightier to Save
+    @cache_result("bootstrap_samples_idxs.data")
+    def compute_bootstrap_samples_idxs():
+        return generate_bootstrap_samples_idxs(14, sample_amount)
+    
+    bootstrap_samples_idxs = compute_bootstrap_samples_idxs()
+        
+    ### Processing OOB Results For Samples (each method in each color space) ---
+    for color_space in color_space_oriented_df.keys():
+        
+        methods = color_space_oriented_df[color_space]
+        oob_362_plus_results_over_methods[color_space] = {}
+        
+        for method_name in methods.keys():
+            method_ds: condo_ds_t = color_space_oriented_df[color_space][method_name]
+            
+            method_oob_corrected_ret = oob_362_quality_prop_measurement(method_ds, bootstrap_samples_idxs, f_to_get_metric_from_condo_ds)
+            oob_362_plus_results_over_methods[color_space][method_name] = method_oob_corrected_ret
+            
+    return oob_362_plus_results_over_methods
